953692e53d63bda1ac888488f389102a19b98d41
sushi-chaaaan <mail@sushichan.live>
2023-10-07 06:00:57 +0900

    build: add tailwind-variants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        "1200/630": "1200 / 630",
+        "16/9": "16 / 9",
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+        "1/1": "1 / 1",
+      },
+    },
   },
   plugins: [],
 }
