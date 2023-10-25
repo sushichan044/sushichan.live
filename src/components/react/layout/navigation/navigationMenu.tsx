5b76063fef4f5c2b978f756071133f4374faadfc
sushi-chaaaan <mail@sushichan.live>
2023-10-26 02:01:32 +0900

    feat: add navigation menu

---
+++ src/components/react/layout/navigation/navigationMenu.tsx
@@ -0,0 +1,51 @@
+"use client"
+import * as NavMenu from "@radix-ui/react-navigation-menu"
+import React from "react"
+import type { ComponentProps } from "react"
+
+import styles from "@/components/react/layout/navigation/styles.module.scss"
+
+type NavLinkProps = ComponentProps<typeof NavMenu.Link> & {
+  topPath: string
+}
+
+const NavLink: React.FC<NavLinkProps> = ({
+  href,
+  children,
+  topPath,
+  ...props
+}) => {
+  const isActive = topPath === href
+
+  return (
+    <NavMenu.Link
+      active={isActive}
+      asChild
+      className={styles.indicator}
+      {...props}
+    >
+      <a href={href}>{children}</a>
+    </NavMenu.Link>
+  )
+}
+
+const NavigationMenu = ({ topPath }: { topPath: string }) => {
+  return (
+    <NavMenu.Root className={styles["menu-root"]}>
+      <NavMenu.List className={styles["menu-list"]}>
+        <NavMenu.Item>
+          <NavLink href="/about" topPath={topPath}>
+            <p>About</p>
+          </NavLink>
+        </NavMenu.Item>
+        <NavMenu.Item>
+          <NavLink href="/blog" topPath={topPath}>
+            <p>Blog</p>
+          </NavLink>
+        </NavMenu.Item>
+      </NavMenu.List>
+    </NavMenu.Root>
+  )
+}
+
+export default NavigationMenu
