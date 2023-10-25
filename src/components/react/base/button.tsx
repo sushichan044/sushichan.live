0462e81b7216513b46878784028f003002c0a213
sushi-chaaaan <mail@sushichan.live>
2023-10-25 14:17:55 +0900

    feat: add base button

---
+++ src/components/react/base/button.tsx
@@ -0,0 +1,25 @@
+import type { PropsWithoutStyle } from "@/utils/@types/propsWithoutStyle"
+import type React from "react"
+import { tv, type VariantProps } from "tailwind-variants"
+
+const style = tv({})
+
+export type ButtonVariants = VariantProps<typeof style>
+
+type ButtonProps = Omit<PropsWithoutStyle<"button", "react">, "role"> & {
+  role: React.HTMLAttributes<HTMLButtonElement>["role"]
+}
+type Props = ButtonProps & {
+  variants?: ButtonVariants
+  children: React.ReactNode
+}
+
+const Button = ({ children, onClick, ...props }: Props) => {
+  return (
+    <button onClick={onClick} {...props}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
