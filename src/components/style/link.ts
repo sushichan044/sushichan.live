e89c395bd13f5f564cac027332027ca43c94eff7
sushi-chaaaan <mail@sushichan.live>
2023-10-30 17:33:49 +0900

    fix: use compound variants in link

--- src/components/style/link.ts
+++ src/components/style/link.ts
@@ -7,14 +7,31 @@ const LinkStyle = tv({
       neutral: "text-light dark:text-dark",
     },
     hoverColor: {
-      primary: "transition-colors duration-200 hover:text-blue-600",
-      neutral: "transition-colors duration-200 hover:text-blue-600",
+      primary: "transition-colors duration-200",
+      neutral: "transition-colors duration-200",
     },
     decoration: {
       underline: "underline",
       none: "no-underline",
     },
   },
+  compoundVariants: [
+    {
+      color: "primary",
+      class: "hover:text-blue-600",
+    },
+    {
+      color: "neutral",
+      hoverColor: "primary",
+      class: "hover:text-blue-500",
+    },
+    {
+      color: "neutral",
+      hoverColor: "neutral",
+      class:
+        "hover:text-custom-little-light dark:hover:text-custom-little-dark",
+    },
+  ],
   defaultVariants: {
     color: "neutral",
     decoration: "underline",
