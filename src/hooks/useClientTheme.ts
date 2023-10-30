e49dc3df3d2de39375fd5cbeff8a516a045aa986
sushi-chaaaan <mail@sushichan.live>
2023-10-30 23:30:08 +0900

    chore: run eslint

--- src/hooks/useClientTheme.ts
+++ src/hooks/useClientTheme.ts
@@ -1,25 +1,25 @@
-import 'client-only'
+import "client-only"
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 const useClientTheme = () => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light')
+  const [theme, setTheme] = useState<"light" | "dark">("light")
 
-  const setDark = () => setTheme('dark')
-  const setLight = () => setTheme('light')
-  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+  const setDark = () => setTheme("dark")
+  const setLight = () => setTheme("light")
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light")
 
   useEffect(() => {
     const onChange = (e: MediaQueryListEvent) => {
-      setTheme(e.matches ? 'dark' : 'light')
+      setTheme(e.matches ? "dark" : "light")
     }
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-    mediaQuery.addEventListener('change', onChange)
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
+    mediaQuery.addEventListener("change", onChange)
 
     // set initial value
-    setTheme(mediaQuery.matches ? 'dark' : 'light')
+    setTheme(mediaQuery.matches ? "dark" : "light")
     // clean up
-    return () => mediaQuery.removeEventListener('change', onChange)
+    return () => mediaQuery.removeEventListener("change", onChange)
   }, [])
 
   return { theme, setDark, setLight, toggleTheme }
