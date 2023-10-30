75f59140883897f6d5449ba2eb204041f4aae634
sushi-chaaaan <mail@sushichan.live>
2023-10-31 00:21:55 +0900

    release: switch SITE_URL to root domain🎉

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://beta.sushichan.live"
-export const SITE_TITLE = "Astro Blog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_URL = "https://www.sushichan.live"
