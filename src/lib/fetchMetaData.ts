8ab7d8d8aa344fc90773f2cc027afee4f2602128
sushi-chaaaan <mail@sushichan.live>
2023-12-18 02:04:49 +0900

    fix: remove OGS

--- src/lib/fetchMetaData.ts
+++ src/lib/fetchMetaData.ts
@@ -1,17 +1,8 @@
-import type { OgObject } from "open-graph-scraper/dist/lib/types"
-
 import scrape from "fetch-site-metadata"
-import OGS from "open-graph-scraper"
 
 const fetchMetaData = async (url: string) => {
   const res = await scrape(url)
   return res
 }
 
-const fetchMetaDataWithOGS = async (url: string): Promise<OgObject | null> => {
-  const options = { url }
-  const { error, result } = await OGS(options)
-  return error ? null : result
-}
-
-export { fetchMetaData, fetchMetaDataWithOGS }
+export { fetchMetaData }
