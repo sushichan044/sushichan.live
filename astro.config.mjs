07716a793c509b826cb5c57bc1c2bdb4abbf01da
sushi-chaaaan <mail@sushichan.live>
2023-12-17 16:37:50 +0900

    build: prepare replace url card fetcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -95,7 +95,7 @@ export default defineConfig({
       minify: false,
     },
     ssr: {
-      noExternal: ["react-tweet", "fetch-site-metadata"],
+      noExternal: ["react-tweet", "fetch-site-metadata", "open-graph-scraper"],
     },
   },
 })
--- src/lib/fetchMetaData.ts
+++ src/lib/fetchMetaData.ts
@@ -1,8 +1,17 @@
+import type { OgObject } from "open-graph-scraper/dist/lib/types"
+
 import scrape from "fetch-site-metadata"
+import OGS from "open-graph-scraper"
 
 const fetchMetaData = async (url: string) => {
   const res = await scrape(url)
   return res
 }
 
-export default fetchMetaData
+const fetchMetaDataWithOGS = async (url: string): Promise<OgObject | null> => {
+  const options = { url }
+  const { error, result } = await OGS(options)
+  return error ? null : result
+}
+
+export { fetchMetaData, fetchMetaDataWithOGS }
