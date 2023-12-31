24360f9e3992b5fd46d1f2d1e3b582f08fc30b04
sushi-chaaaan <mail@sushichan.live>
2023-12-31 14:15:57 +0900

    feat: upgrade metadata fetch lib, set UA

--- src/lib/fetchMetaData.ts
+++ src/lib/fetchMetaData.ts
@@ -1,7 +1,12 @@
 import scrape from "fetch-site-metadata"
 
 const fetchMetaData = async (url: string) => {
-  const res = await scrape(url)
+  const res = await scrape(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+    },
+  })
   return res
 }
 
