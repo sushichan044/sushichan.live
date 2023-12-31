f8a665821800d5dc4f1666cc339e559fc44b637b
sushi-chaaaan <mail@sushichan.live>
2023-12-31 16:17:42 +0900

    fix: I hate all of world

--- src/lib/fetchMetaData.ts
+++ src/lib/fetchMetaData.ts
@@ -1,13 +1,16 @@
 import scrape from "fetch-site-metadata"
 
 const fetchMetaData = async (url: string) => {
-  const res = await scrape(url, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
-    },
-    suppressAdditionalRequest: true,
-  })
+  const res = await scrape(
+    url,
+    // {
+    // headers: {
+    //   "User-Agent":
+    //     "Mozilla/5.0 (Macintosh; U; +Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+    // },
+    // suppressAdditionalRequest: true,
+    // }
+  )
   return res
 }
 
