e6e1d61554739b186e9e2ba17194e36975c4a68d
sushi-chaaaan <mail@sushichan.live>
2024-01-14 21:00:00 +0900

    feat: update portfolio

--- src/pages/portfolio/data.ts
+++ src/pages/portfolio/data.ts
@@ -2,10 +2,27 @@ import type { HTMLAttributes } from "astro/types"
 
 type Experience = {
   description: string
-  time: {
-    attr: HTMLAttributes<"time">["datetime"]
-    label: string
-  }
+  time:
+    | {
+        data: {
+          attr: HTMLAttributes<"time">["datetime"]
+          label: string
+        }
+        range: false
+      }
+    | {
+        data: {
+          from: {
+            attr: HTMLAttributes<"time">["datetime"]
+            label: string
+          }
+          to?: {
+            attr: HTMLAttributes<"time">["datetime"]
+            label: string
+          }
+        }
+        range: true
+      }
   title: string
 }
 
@@ -14,17 +31,34 @@ const EXPERIENCES = [
     description:
       "チャットアプリ上で使うBotの作成をきっかけに、書いたものが動いて人の助けになることに魅力を感じ、プログラミングを学ぶようになりました。",
     time: {
-      attr: "2022-04",
-      label: "2022年4月",
+      data: {
+        attr: "2022-03",
+        label: "2022年3月",
+      },
+      range: false,
     },
     title: "プログラミングに出会う",
+  },
+  {
+    description: "",
+    time: {
+      data: {
+        attr: "2022-04",
+        label: "2022年4月",
+      },
+      range: false,
+    },
+    title: "電気通信大学 入学",
   },
   {
     description:
       "完成には至りませんでしたが、なにもないところからアプリケーションやAPIを組んでいく楽しさを知りました。この頃から本格的に色々な学習をするようになりました。",
     time: {
-      attr: "2022-10",
-      label: "2022年10月",
+      data: {
+        attr: "2022-10",
+        label: "2022年10月",
+      },
+      range: false,
     },
     title: "初めてハッカソンに挑戦する",
   },
@@ -32,10 +66,45 @@ const EXPERIENCES = [
     description:
       "Webフロントエンドの開発に触れ、作ったものが世界中どこからでも見れる/使えることに感動しました。この頃から主にフロントエンドの学習や開発をしています。",
     time: {
-      attr: "2023-01",
-      label: "2023年1月",
+      data: {
+        attr: "2023-01",
+        label: "2023年1月",
+      },
+      range: false,
     },
     title: "Webフロントエンドに出会う",
+  },
+  {
+    description:
+      "第73回調布祭実行委員会に所属し、新歓特設サイトや調布祭公式サイトの制作を主導しました。あわせてインフラ基盤の刷新も行い、大幅な高速化・省力化に成功しました。",
+    time: {
+      data: {
+        from: {
+          attr: "2023-04",
+          label: "2023年4月",
+        },
+        to: {
+          attr: "2023-11",
+          label: "2023年11月",
+        },
+      },
+      range: true,
+    },
+    title: "調布祭実行委員会(電気通信大学) 技術担当",
+  },
+  {
+    description:
+      "調布祭実行委員会での経験を活かし、Webサイトの改修やクラウド基盤の更新による費用削減に取り組んでいます。",
+    time: {
+      data: {
+        from: {
+          attr: "2023-11",
+          label: "2023年11月",
+        },
+      },
+      range: true,
+    },
+    title: "バーチャルライブ研究会(電気通信大学)",
   },
 ] as const satisfies Experience[]
 
