93884b5d302b2979080a94ceecb080abfb8830d7
sushi-chaaaan <mail@sushichan.live>
2024-01-19 05:26:21 +0900

    fix: define __dirname

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,9 @@
-import path from "path"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
 import { defineConfig } from "vitest/config"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   test: {
