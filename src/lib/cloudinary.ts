d541a0027bec80c6610cf0fefacab47fe56155c8
sushi-chaaaan <mail@sushichan.live>
2024-02-03 18:23:01 +0900

    fix: fetch header

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -76,7 +76,12 @@ export const getCloudinaryImageSize = async (
   }
 
   try {
-    const res = await fetch(cloudinaryMetadataLoader(url))
+    const res = await fetch(cloudinaryMetadataLoader(url), {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; +Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
     const isJson = res.headers.get("content-type")?.includes("application/json")
     const data = isJson ? await res.json() : await res.text()
     if (res.ok && isJson) {
