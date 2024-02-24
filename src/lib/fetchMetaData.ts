6bf59e55c64b2fc04a0c5e1fc0d950d6d519f8c1
sushi-chaaaan <mail@sushichan.live>
2024-02-24 20:38:12 +0900

    Revert "fix(try): metadata fetch"
    This reverts commit 0f6f01c7567d8cde7efcf3ae66ac2b6ae9b3ba8c.

--- src/lib/fetchMetaData.ts
+++ src/lib/fetchMetaData.ts
@@ -10,13 +10,16 @@ const fetchMetaData = async (url: string) => {
     return cached
   }
 
-  const metaDataResponse = await scrape(url, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Macintosh; U; +Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
-    },
-    suppressAdditionalRequest: true,
-  })
+  const metaDataResponse = await scrape(
+    url,
+    //    {
+    //   headers: {
+    //     "User-Agent":
+    //       "Mozilla/5.0 (Macintosh; U; +Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+    //   },
+    //   suppressAdditionalRequest: true,
+    // }
+  )
 
   METADATA_CACHE.set(url, metaDataResponse)
   return metaDataResponse
