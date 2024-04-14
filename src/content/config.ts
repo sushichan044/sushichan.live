45fba08c5e51b76ac0368008ecb420ae837ba645
sushi-chaaaan <mail@sushichan.live>
2024-04-14 22:56:34 +0900

    refactor: split base schema and integrate blog, presentation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,36 @@
 import { defineCollection, reference, z } from "astro:content"
 
+const baseSchema = z.object({
+  createdAt: z.coerce.date(),
+  tags: z.array(z.string()).optional(),
+  title: z.string(),
+  updatedAt: z.coerce.date().optional(),
+})
+
 const posts = defineCollection({
-  schema: z.object({
-    alert: z.array(z.string()).optional(),
-    createdAt: z.coerce.date(),
-    description: z.string(),
-    relatedPosts: reference("posts").array().optional(),
-    status: z
-      .enum(["draft", "published", "private", "preview"])
-      .default("draft"),
-    tags: z.array(z.string()).optional(),
-    thumbnail: z.string().url(),
-    title: z.string(),
-    updatedAt: z.coerce.date().optional(),
-  }),
+  schema: baseSchema.merge(
+    z.object({
+      alert: z.array(z.string()).optional(),
+      description: z.string(),
+      relatedPosts: reference("posts").array().optional(),
+      status: z
+        .enum(["draft", "published", "private", "preview"])
+        .default("draft"),
+      thumbnail: z.string().url(),
+      type: z.literal("blog"),
+    }),
+  ),
   type: "content",
 })
 
-export const collections = { posts: posts }
+const presentations = defineCollection({
+  schema: baseSchema.merge(
+    z.object({
+      type: z.literal("presentation"),
+      url: z.string().url(),
+    }),
+  ),
+  type: "data",
+})
+
+export const collections = { posts: posts, presentations: presentations }
