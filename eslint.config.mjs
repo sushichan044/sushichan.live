9328411e7ab6fa1c1171d533086552e4af41de94
sushichan044 <mail@sushichan.live>
2024-10-26 23:16:48 +0900

    fix: error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,11 @@
 import astro from "@virtual-live-lab/eslint-config/presets/astro";
 import react from "@virtual-live-lab/eslint-config/presets/react";
+import tseslint from "typescript-eslint";
 
-export default [...astro, ...react];
+export default tseslint.config(
+  {
+    ignores: ["worker-configuration.d.ts"],
+  },
+  ...astro,
+  ...react,
+);
