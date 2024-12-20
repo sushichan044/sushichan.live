e9e587e57be69ef3ada6bf42b73fd5704e34cc74
sushichan044 <mail@sushichan.live>
2024-12-21 00:30:56 +0900

    getEntry 返り値を null チェックする

--- packages/sushichan.live/src/pages/blog/og-image/[...slug].png.ts
+++ packages/sushichan.live/src/pages/blog/og-image/[...slug].png.ts
@@ -20,6 +20,10 @@ export const GET: APIRoute<
   { slug: TrimExtension<CollectionEntry<"posts">["id"]> }
 > = async ({ params: { slug } }) => {
   const post = await getEntry("posts", slug);
+  if (post == null) {
+    return new Response("Not found", { status: 404 });
+  }
+
   const png = await getBlogOGImage({ post });
 
   return new Response(png);
