8826af8c50e067bdb7fa4ae04809fb20fc3e7fb3
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-04-19 00:23:34 +0900

    dbg

--- packages/sushichan.live/src/middleware.ts
+++ packages/sushichan.live/src/middleware.ts
@@ -2,6 +2,9 @@ import { sequence, defineMiddleware } from "astro:middleware";
 
 const redirectIfNotFound = defineMiddleware(async (c, next) => {
   const res = await next();
+
+  console.log("From Middleware", res.status);
+
   if (res.status === 404) {
     return c.rewrite("/404");
   }
