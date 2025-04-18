a86a09a3748b06443bf6bdd88ebd4bacd5c668ef
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-04-19 00:12:33 +0900

    add middleware

---
+++ packages/sushichan.live/src/middleware.ts
@@ -0,0 +1,12 @@
+import { sequence, defineMiddleware } from "astro:middleware";
+
+const redirectIfNotFound = defineMiddleware(async (c, next) => {
+  const res = await next();
+  if (res.status === 404) {
+    return c.redirect("/404");
+  }
+
+  return res;
+});
+
+export const onRequest = sequence(redirectIfNotFound);
