1a3e998f3fab16d49e2260b0f5e9319aa084b457
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-04-19 00:16:46 +0900

    Use Astro rewrite

--- packages/sushichan.live/src/middleware.ts
+++ packages/sushichan.live/src/middleware.ts
@@ -3,7 +3,7 @@ import { sequence, defineMiddleware } from "astro:middleware";
 const redirectIfNotFound = defineMiddleware(async (c, next) => {
   const res = await next();
   if (res.status === 404) {
-    return c.redirect("/404");
+    return c.rewrite("/404");
   }
 
   return res;
